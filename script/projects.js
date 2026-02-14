81da29f9da9f9229f1935523483ae08bfffe5c6a
Balasubramanyam <cheruvupallibalasubramanyam@gmail.com>
2026-02-14 15:46:48 +0530

    changed

--- script/projects.js
+++ script/projects.js
@@ -7,7 +7,7 @@ const projects=[
     desc:"A simple CRUD application built using the MERN stack that allows users to create, read, update, and delete data with a clean UI and RESTful APIs.",
     tech:["React","Bootstrap","Node.js","Express.js","MongoDb","Figma"],
     git:"https://github.com/JayavardhanGundala/CRUD-MERN-stack",
-    demo:"https://crudproject-ys0f.onrender.com/",
+    demo:"f",
     featured:true
 
 },
@@ -147,8 +147,7 @@ project.tech.forEach(t => {
                     </div>
                     <div class="links">
                         <a href=${project.git} target="_blank"><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github h-6 w-6" aria-hidden="true"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>Code</button></a>
-                        <a href=${project.demo} target="blank"><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play w-4 h-4 mr-2 group-hover/btn:scale-110 transition-transform" aria-hidden="true"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"></path></svg>Demo</button></a>
-                    </div>
+                       
                 </div>
             </div>`
 
@@ -214,7 +213,7 @@ project.tech.forEach(t => {
                     </div>
                     <div class="links">
                         <a href=${project.git} target="_blank"><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github h-6 w-6" aria-hidden="true"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>Code</button></a>
-                        <a href=${project.demo} target="blank"><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play w-4 h-4 mr-2 group-hover/btn:scale-110 transition-transform" aria-hidden="true"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"></path></svg>Demo</button></a>
+                      
                     </div>
                 </div>
             </div>`
